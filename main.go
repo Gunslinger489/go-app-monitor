611df02d7b1d3dcfa69b0f957bd5b00686024e87
Lucas Marques <lucasg3.gdh@gmail.com>
2023-01-31 09:14:46 -0300

    Code improved

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -31,7 +36,7 @@ func leOpcao() int {
 	var opcaoLida int
 
 	fmt.Scan(&opcaoLida)
-	fmt.Println("A opção escolhida foi", opcaoLida)
+	//fmt.Println("A opção escolhida foi", opcaoLida)
 
 	return opcaoLida
 
@@ -43,7 +48,7 @@ func exibeMenu() {
 	fmt.Println("1 - Iniciar o monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("3 - Sair")
-	fmt.Print("Digite uma opção:")
+	fmt.Print("Digite uma opção: ")
 }
 
 func init() {
@@ -58,7 +63,8 @@ func init() {
 func iniciaMonitoramento() {
 
 	fmt.Println("Monitorando ...")
-	sites := []string{"https://butia.rs.gov.br", "https://google.com.br", "https://terra.com.br", "https://httpstat.us/404"}
+
+	sites := leSitesDoArquivo()
 
 	for i := 0; i < monitoramentos; i++ {
 		for i, site := range sites {
@@ -68,20 +74,83 @@ func iniciaMonitoramento() {
 		time.Sleep(delay * time.Second)
 		fmt.Println("")
 	}
-
 	fmt.Println("")
-
+	fmt.Println("Fim dos testes!")
 }
 
 func testaSite(site string) {
 
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
 
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	}
+
+	//Trata Status Code
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLogs(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas.")
+		registraLogs(site, false)
 	}
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+
+	// Abre o arquivo
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	// Lê o arquivo linha a linha
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	// Fecha o arquivo
+	arquivo.Close()
+	return sites
+
+}
+
+func registraLogs(site string, status bool) {
+	
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- online: " + strconv.FormatBool(status) + "\n")
+	
+	arquivo.Close()
+
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
 
 func main() {
@@ -98,12 +167,13 @@ func main() {
 			iniciaMonitoramento()
 		case 2:
 			fmt.Println("Exibindo logs ...")
+			imprimeLogs()
 		case 3:
 			fmt.Println("Saindo do programa ...")
 			os.Exit(0)
 		default:
 			fmt.Println("Opção inválida. Digite novamente.")
-			os.Exit(-1)
+			continue
 		}
 	}
 }
